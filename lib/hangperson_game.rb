f8762dddfe6a7c06a14430e519f04e11be53196f
EC2 Default User <ec2-user@ip-172-31-42-215.eu-west-1.compute.internal>
2019-02-12 21:10:16 +0000

    finished part2 practice

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -16,11 +16,47 @@ class HangpersonGame
     @word = word
     @guesses = ''
     @wrong_guesses = ''
+    @current_status = :play
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
+  
+  def guess(alphabet)
+   
+    raise ArgumentError if !alphabet || alphabet.empty? || !alphabet.match(/[A-Za-z]/)
+    # alphabet chars to downcase
+    alphabet.downcase!
+
+  return false if @guesses.index(alphabet) || @wrong_guesses.index(alphabet)
+  if word.index(alphabet)
+      @guesses << alphabet
+    else
+      @wrong_guesses << alphabet
+    end 
+  end
+
+  def word_with_guesses
+    displayed = ''
+    @word.each_char do |alphabet|
+      if @guesses.index(alphabet)
+        displayed << alphabet
+      else
+        displayed << '-'
+      end
+    end
+    displayed
+  end
+  
+  def check_win_or_lose
+    
+    return :lose if @wrong_guesses.length >= 7
+    
+    return :win if !word_with_guesses.index('-')  
+    :play
+  end
+
   def self.get_random_word
     require 'uri'
     require 'net/http'
